0d9b5014bf8d2770931a46239191c34c15325eeb
Hector Monticeli Arantes <hectormonticelia@gmail.com>
2025-01-18 22:46:42 -0300

    ex015

--- aulas/aula12ex/ex015/script.js
+++ aulas/aula12ex/ex015/script.js
@@ -10,12 +10,44 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero  = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'imagens/bebe-m.jpg')
+            } else if (idade < 21) {
+                //Adolescente
+                img.setAttribute('src', 'imagens/jovem-m.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/adulto-m.jpg')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'imagens/idoso-m.jpg')
+            }
         } else {
             gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'imagens/bebe-f.jpg')
+            } else if (idade < 21) {
+                //Adolescente
+                img.setAttribute('src', 'imagens/jovem-f.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/adulto-f.jpg')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'imagens/idoso-f.jpg')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Verificamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
+        img.style.width = '250px'
+        img.style.borderRadius = '50%'
     }
 }
