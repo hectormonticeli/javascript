2ef3c682e47c433e26ccf4187a13f65ef5581a30
Hector Monticeli Arantes <hectormonticelia@gmail.com>
2025-01-21 22:17:16 -0300

    aulas 13 e 14

---
+++ aulas/aula14ex/ex016(meu)/script.js
@@ -0,0 +1,28 @@
+function contar() {
+    var inicio = document.querySelector('input#inicio')
+    var fim = document.querySelector('input#fim')
+    var passo = document.querySelector('input#passo')
+    var res = document.querySelector('div#res')
+    
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        inicio = Number(inicio.value)
+        fim = Number(fim.value)
+        passo = Number(passo.value)
+
+        if (passo.value == 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            passo = 1
+        }
+
+        res.innerHTML = '<p>Contando:</p>'
+
+        for(var c = inicio; c <= fim; c += passo) {
+            res.innerHTML += c
+            res.innerHTML += ' --> '
+        }
+
+        res.innerHTML += 'fim'
+    }
+}
